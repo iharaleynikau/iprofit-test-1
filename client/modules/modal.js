3f9b5be95b2997e6d15cd634829ee041acb22c91
Igor <victororlov174@gmail.com>
2024-02-10 18:23:21 +0300

    modal-window scrollbar fix

--- client/modules/modal.js
+++ client/modules/modal.js
@@ -3,12 +3,18 @@ const modal = () => {
 
   const btn = document.getElementById('modal-button');
 
-  const body = document.getElementsByTagName('body')[0];
+  const body = document.getElementsByTagName('html')[0];
 
   const span = document.getElementsByClassName('close')[0];
 
   btn.onclick = () => {
+    const vs = body.scrollHeight > body.clientHeight;
     modal.style.display = 'block';
+
+    if (vs) {
+      body.classList.add('scrollbar-gutter');
+    }
+
     body.classList.add('hide-overflow');
   };
 
